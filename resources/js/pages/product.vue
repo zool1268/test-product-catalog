e1638ea66565451201ff38cf0ddae0c2bdfcd4d2
Alexey <zool@inetss.com>
2026-01-28 00:23:32 +0700

    добавил продуктовую страницу

---
+++ resources/js/pages/product.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import useApiQuery from '@/composables/useApiQuery';
+import type { Product } from '@/types';
+import { router } from '@inertiajs/vue3'
+import { computed } from 'vue';
+
+const props = defineProps<{
+    id: string
+}>()
+
+const { data, loading } = useApiQuery<{ id: string }, { data: Product }>(`products/${props.id}`)
+const product = computed(() => data.value?.data)
+</script>
+
+<template lang="pug">
+    q-card
+        q-card-section Категория: {{ product?.category.name }}
+        q-card-section
+            h3 {{ product?.name }}
+        q-card-section {{ product?.description }}
+        q-card-section Цена: {{ product?.formatted_price }}
+        q-card-actions
+            q-btn(color="primary" @click.prevent="router.visit('/')") << к списку товаров
+        q-inner-loading(:showing="loading")
+</template>
